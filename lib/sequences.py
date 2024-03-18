d44f7727f204c3ab479ac698f473343339016312
Eric-Martin <gaspar.ericmartin@gmail.com>
2024-03-17 22:45:23 -0400

    Complete assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_list = []
+
+    if length == 0:
+        print(fib_list)
+    elif length == 1:
+        fib_list.append(0)
+        print(fib_list)
+    else:
+        fib_list.extend([0,1])
+        for x in range(length - 2):
+            fib_list.append(fib_list[len(fib_list) -2] + fib_list[len(fib_list) - 1])
+        print(fib_list)
